6099189e43dc24439f3f40786ccd026272054c4c
Adam Lesinski <adamlesinski@google.com>
2014-11-11 11:12:01 -0800

    Remove density from PRODUCT_AAPT_CONFIG
    AAPT ignores densities in PRODUCT_AAPT_CONFIG.
    The use of PRODUCT_AAPT_PREF_CONFIG for density is
    encouraged, as AAPT is able to determine the fallback
    density to use if a resource of the specified density
    does not exist.
    
    Change-Id: If2b43a44593bc234118045d03bd48cd9820dc05a

--- find7s.mk
+++ find7s.mk
@@ -23,7 +23,7 @@ TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
 # Device uses high-density artwork where available
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi xxxhdpi
+PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 
 # call dalvik heap config
